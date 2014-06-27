aac721c0f3dd6427dff4d9f2243b53b7de7bcd32
StefanScherer <scherer_stefan@icloud.com>
2014-06-27 18:49:53 +0200

    improved RDP serverspec tests

--- test/vcloud_spec.rb
+++ test/vcloud_spec.rb
@@ -1,16 +1,21 @@
 require_relative 'spec_helper'
 
 describe 'box' do
+  # this tests if rsync works from bin/test-box-vcloud.bat
   describe file('c:/vagrant/testdir/testfile.txt') do
     it { should be_file }
     it { should contain "Works" }
   end
 
+  # check SSH
   describe service('OpenSSH Server') do
     it { should be_installed  }
     it { should be_enabled  }
     it { should be_running  }
     it { should have_start_mode("Automatic")  }
+  end
+  describe port(22) do
+    it { should be_listening  }
   end
 
   describe service('VMware Tools') do
@@ -20,16 +25,25 @@ describe 'box' do
     it { should have_start_mode("Automatic")  }
   end
 
-  describe port(22) do
+
+  # check WinRM
+  describe port(5985) do
     it { should be_listening  }
   end
 
+  # check RDP
   describe port(3389) do
     it { should be_listening  }
   end
-
-  describe port(5985) do
-    it { should be_listening  }
+  describe windows_registry_key('HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Terminal Server\fDenyTSConnections') do
+    it { should exist  }
+    it { should have_property('dword value', :type_dword)  }
+    it { should have_value('0')  }
+  end
+  describe windows_registry_key('HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\UserAuthentication') do
+    it { should exist  }
+    it { should have_property('dword value', :type_dword)  }
+    it { should have_value('0')  }
   end
 
   # check for 10 GBit vmxnet3 network adapter
