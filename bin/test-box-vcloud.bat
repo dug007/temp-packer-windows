53aee2f16a2dabee766507d5f3cfd3eefc15112c
StefanScherer <scherer_stefan@icloud.com>
2014-06-28 14:11:52 +0200

    remove known_hosts and show some details inside jenkins build

--- bin/test-box-vcloud.bat
+++ bin/test-box-vcloud.bat
@@ -49,6 +49,10 @@ ping 1.1.1.1 -n 1 -w 240000 > nul
 pushd %tmp_path%
 call :create_vagrantfile
 set VAGRANT_LOG=debug
+echo USERPROFILE = %USERPROFILE%
+if exist %USERPROFILE%\.ssh\known_hosts type %USERPROFILE%\.ssh\known_hosts
+del /Y %USERPROFILE%\.ssh\known_hosts
+if exist %USERPROFILE%\.ssh\known_hosts echo known_hosts still here!!
 vagrant up --provider=%vagrant_provider%
 if ERRORLEVEL 1 goto :done
 
